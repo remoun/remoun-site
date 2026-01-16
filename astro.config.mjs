5348a9dced45342715a8e91f0a6043409993b528
Remoun Metyas <remoun.metyas@gmail.com>
2026-01-15 21:12:47 -0500

    Convert /love and /resume to MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: 'https://remoun.me',
-  integrations: [react()],
+  integrations: [react(), mdx()],
 
   markdown: {
     shikiConfig: {
